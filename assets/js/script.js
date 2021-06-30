bcb9d407193906e4737c43b1bd2738996aedb212
kimle-94 <79667236+kimle-94@users.noreply.github.com>
2021-06-30 17:48:36 -0400

    FINAL2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,3 @@
-// let menu = document.querySelector("#menu");
-// let isdown = false;
-// menu.addEventListener("click", () => {
-//   isdown = !isdown;
-//   if (isdown) {
-//     menu.src = "/assets/svg/menu-red.svg";
-//   } else {
-//     menu.src = "/assets/svg/menu-black.svg";
-//   }
-
-//   let invisible = document.querySelector("#invisible");
-//   invisible.classList.toggle("visible");
-// });
-
-$(function () {
-  $("body#work-page").mousewheel(function (event, delta) {
-    // event.preventDefault();
-    let menu_box = document.querySelector(".menu-box-container");
-    menu_box.scrollLeft -= delta * 1;
-  });
-});
-
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
@@ -34,21 +12,21 @@ window.onscroll = function () {
 let showNavbar = false;
 function toggleMenu() {
   showNavbar = !showNavbar;
-  $('#css-navbar .menu ul').toggleClass('show')
-  let img = $('#menu');
+  $("#css-navbar .menu ul").toggleClass("show");
+  let img = $("#menu");
   if (showNavbar) {
-    img.attr('src', '/assets/svg/exit.svg');
+    img.attr("src", "/assets/svg/exit.svg");
   } else {
-    img.attr('src', '/assets/svg/menu.svg');
+    img.attr("src", "/assets/svg/menu.svg");
   }
 }
 
-$('#navbar-show').click(function(e) {
+$("#navbar-show").click(function (e) {
   e.preventDefault();
 
   toggleMenu();
-})
+});
 
-$('#css-navbar .menu  ul li a').click(function() {
+$("#css-navbar .menu  ul li a").click(function () {
   toggleMenu();
-})
+});
